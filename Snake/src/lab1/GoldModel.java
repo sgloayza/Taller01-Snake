90016aaf89abda9bdaeaa81ca62101b8a07d27f4
NataliaMw <mnmawyin@espol.edu.ec>
2020-10-22 00:40:17 -0500

    Parte Integrante 4

--- Snake/src/lab1/GoldModel.java
+++ Snake/src/lab1/GoldModel.java
@@ -55,8 +55,7 @@ public class GoldModel extends GameModel {
 	 */
 
 	/** Graphical representation of a coin. */
-	private static final GameTile COIN_TILE = new RoundTile(new Color(255, 215,
-			0),
+	private static final GameTile COIN_TILE = new RoundTile(Color.blue,
 			new Color(255, 255, 0), 2.0);
 
 	
